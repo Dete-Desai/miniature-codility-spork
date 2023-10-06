ab45513ffafab54f962ede788663c81c20263408
TheRickFred <naest2473@gmail.com>
2023-10-06 09:43:00 +0300

    updates on golang

---
+++ go/GoMod/Mod-Code-Depend/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Dete-Desai/puppy"
+)
+
+func main() {
+	s1 := puppy.Bark
+	s2 := puppy.Barks()
+	fmt.Println(s1)
+	fmt.Println((s2))
+}
---
+++ go/GoMod/go.mod
@@ -0,0 +1,5 @@
+module mymodule
+
+go 1.21.1
+
+require github.com/Dete-Desai/puppy v0.0.0-20231005153138-eb94ae97e6ec // indirect
