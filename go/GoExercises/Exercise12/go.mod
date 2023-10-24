8ffa450430400a0bf4a6b96c2e2d83e0d8158df6
TheRickFred <naest2473@gmail.com>
2023-10-24 18:09:10 +0300

    updates on GoMods Exercises

---
+++ go/GoExercises/Exercise12/go.mod
@@ -0,0 +1,7 @@
+module mymodule
+
+go 1.21.1
+
+require github.com/Dete-Desai/puppy v0.0.0-20231017135930-c94af6541283
+
+require github.com/GoesToEleven/dog v0.0.0-20230428023317-90bef1c76cb9 // indirect
---
+++ go/GoExercises/Exercise14/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Dete-Desai/puppy"
+)
+
+func main() {
+	fmt.Println("Hello Gophers")
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	s3 := puppy.BigBark()
+	s4 := puppy.BigBarks()
+	fmt.Println(s3)
+	fmt.Println(s4)
+
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+}
