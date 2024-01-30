9611e112a88de58eab26c6c429f07e19945f4e11
TheRickFred <naest2473@gmail.com>
2024-01-30 18:02:58 +0300

    updates on events

--- javascript/Reactjs/01-starting-project/src/App.jsx
+++ javascript/Reactjs/01-starting-project/src/App.jsx
@@ -1,28 +1,42 @@
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+import { CORE_CONCEPTS } from "./data";
+import Header from './components/Header/Header';
+import CoreConcept from './components/CoreConcept';
+import TabButton from "./components/TabButton";
 
-function genRandomInt(max){
-  return Math.floor(Math.random() * (max + 1));
-}
 
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concept">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept
+              {...CORE_CONCEPTS[1]}
+            />
+            <CoreConcept
+              {...CORE_CONCEPTS[2]}
+            />
+            <CoreConcept
+              {...CORE_CONCEPTS[3]}
+            />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Exmples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>Sattes</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
---
+++ javascript/Reactjs/01-starting-project/src/components/CoreConcept.jsx
@@ -0,0 +1,9 @@
+function CoreConcept({title, description, image}) {
+    return (
+      <li>
+        <img src={pimage} alt={ptitle} />
+        <h3>{ptitle}</h3>
+        <p>{pdescription}</p>
+      </li>
+    );
+  }
