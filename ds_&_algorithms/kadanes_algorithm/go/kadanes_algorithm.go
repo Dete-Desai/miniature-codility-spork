ea0d3f304c7c8f445c05ccd1055e2c11c7698147
TheRickFred <naest2473@gmail.com>
2024-03-22 19:02:06 +0300

    updates on kadanes algorithm

--- ds_&_algorithms/kadanes_algorithm/go/kadanes_algorithm.go
+++ ds_&_algorithms/kadanes_algorithm/go/kadanes_algorithm.go
@@ -1,3 +1,25 @@
+package main
+
 import "fmt"
 
-func main(int) {}
+func maxSubArraySum(array []int, size int) int {
+	var max_ending_here int = 0
+	var max_so_far int = 0
+
+	for i := 0; i < size; i++ {
+		max_ending_here = max_so_far + array[i]
+
+		if max_ending_here < 0 {
+			max_ending_here = 0
+		}
+
+		if max_so_far < max_ending_here {
+			max_so_far = max_ending_here
+		}
+	}
+	return max_so_far
+}
+
+func main() {
+	fmt.Printf("The value of the maximum sub array sum is:")
+}
