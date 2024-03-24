6a6c3dbeb545f5f2cc345719be59b1224e0ca494
TheRickFred <naest2473@gmail.com>
2024-03-24 16:02:44 +0300

    updates on kadanes algorithm java code

--- ds_&_algorithms/kadanes_algorithm/java/kadanes_algorithm.java
+++ ds_&_algorithms/kadanes_algorithm/java/kadanes_algorithm.java
@@ -1,1 +1,27 @@
 package kadanes_algorithm.java;
+
+public class kadanes_algorithm {
+    static  int maxSubarraySum(int[] array){
+        int max_ending_here = 0;
+        int max_so_far = 0;
+
+        for (int i=0; i< array.length; i++){
+            max_ending_here = max_ending_here + array[i];
+
+            if(max_ending_here < 0){
+                max_ending_here = 0;
+            }
+            if(max_so_far < max_ending_here){
+                max_so_far = max_ending_here;
+            }
+        }
+        return max_so_far;
+    }
+
+    public static void main(String[] args){
+        int[] input_array = {-2, 1, -3, 4, -1, 2, 1, -5, 4}; 
+        int max_sum = maxSubarraySum(input_array);
+
+        System.out.println("The value of the maximum sum of sub arrays in in the given array is:" + max_sum);
+    }
+}
