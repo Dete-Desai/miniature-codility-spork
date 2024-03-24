9710cd69c3976209437136402d3a4a7eb61e1355
TheRickFred <naest2473@gmail.com>
2024-03-24 14:14:32 +0300

    updates on golang kadanes algorithm

--- ds_&_algorithms/kadanes_algorithm/go/kadanes_algorithm.go
+++ ds_&_algorithms/kadanes_algorithm/go/kadanes_algorithm.go
@@ -2,17 +2,16 @@ package main
 
 import "fmt"
 
-func maxSubArraySum(array []int, size int) int {
+func maxSubArraySum(array []int) int {
 	var max_ending_here int = 0
 	var max_so_far int = 0
 
-	for i := 0; i < size; i++ {
-		max_ending_here = max_so_far + array[i]
+	for _, value := range array {
+		max_ending_here += value
 
 		if max_ending_here < 0 {
 			max_ending_here = 0
 		}
-
 		if max_so_far < max_ending_here {
 			max_so_far = max_ending_here
 		}
@@ -21,5 +20,9 @@ func maxSubArraySum(array []int, size int) int {
 }
 
 func main() {
-	fmt.Printf("The value of the maximum sub array sum is:")
+	var input_array []int = []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+
+	var max_sum int = maxSubArraySum(input_array)
+
+	fmt.Printf("The maximum sum of sub arrays in the give array is %v", max_sum)
 }
